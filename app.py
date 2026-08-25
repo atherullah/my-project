4f9571da1db6743830b27d65d83a1156c738bb0f
atherullah <athersaif817@gmail.com>
2026-08-25 13:29:48 +0500

    fix app.py

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.rout('/')
+@app.route('/')
 def home():
-	return"hello from Dockerized github action app"
+    return "Hello from Dockerized GitHub Actions app!"
 
-if __name__ ='__main__':
-	app.run(host='0.0.0.0 , port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
